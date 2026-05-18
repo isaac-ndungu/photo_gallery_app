9154a20c264c5325b101d184c92dbd562ddb649b
Isaac <isaac@example.com>
2026-05-18 09:21:10 +0300

    update storages in settings.py

--- galleryproject/settings.py
+++ galleryproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://photo-gallery-app-ge4n.onrender.com/', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -157,7 +157,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        "BACKEND": "cloudinary_storage.storage.StaticHashedCloudinaryStorage",
     },
 }
 
